324c735bf61c87ba3e5944b066cb3affdb3e24f4
NtwaliHeritier <ntwalihatsor78.nh@gmail.com>
2022-05-10 13:41:54 +0200

    create winning_checker module and winning_combo module attribute

---
+++ lib/tic_tac_toe/game/winning_checker.ex
@@ -0,0 +1,12 @@
+defmodule TicTacToe.Game.WinningChecker do
+  @winning_combo [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+  ]
+end
