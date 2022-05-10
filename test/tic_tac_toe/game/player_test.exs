d7d819444a2b2312c88644e8f5c2ef38583ce16d
NtwaliHeritier <ntwalihatsor78.nh@gmail.com>
2022-05-10 23:37:54 +0200

    update unit test for player module with the right params

--- test/tic_tac_toe/game/player_test.exs
+++ test/tic_tac_toe/game/player_test.exs
@@ -7,13 +7,15 @@ defmodule TicTacToe.Game.PlayerTest do
     {:ok, player: %Player{score_combo: [3], name: "Heritier", key: "X"}}
   end
 
-  describe "test the player module" do
-    test "&new/2" do
+  describe "&new/2" do
+    test "creates a new player struct with right params" do
       assert Player.new("Heritier", "X") === %Player{name: "Heritier", key: "X", score_combo: []}
       refute Player.new("Heriier", "X") === %Player{name: "Heritier", key: "X", score_combo: []}
     end
+  end
 
-    test "&update_player_score/2", %{player: player} do
+  describe "&update_player_score/2" do
+    test "updates score of a player", %{player: player} do
       assert Player.update_player_score(player, 1) === %Player{
                score_combo: [1, 3],
                name: "Heritier",
