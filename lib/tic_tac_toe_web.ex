2e94311b6711b46f3603d3e20a89d4afaa1baf4d
NtwaliHeritier <ntwalihatsor78.nh@gmail.com>
2022-05-11 13:38:32 +0200

    add liveview helpers and templates

--- lib/tic_tac_toe_web.ex
+++ lib/tic_tac_toe_web.ex
@@ -80,6 +80,9 @@ defmodule TicTacToeWeb do
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
 
+      # Import LiveView helpers (live_render, live_component, live_patch, etc)
+      import Phoenix.LiveView.Helpers
+
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
 
