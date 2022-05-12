b3096053e795e238a29063e7bf507689fe5bc8a6
NtwaliHeritier <ntwalihatsor78.nh@gmail.com>
2022-05-12 18:48:22 +0200

    add tests for new functions

--- test/tic_tac_toe/game/game_play_test.exs
+++ test/tic_tac_toe/game/game_play_test.exs
@@ -4,13 +4,23 @@ defmodule TicTacToe.Game.GamePlayTest do
   alias TicTacToe.Game.{GamePlay, Player}
   import TicTacToeWeb.Support.GameSupport, only: [create_game: 1]
 
-  describe "&new_game/2" do
+  describe "&new_game/1" do
     setup [:create_game]
 
-    test "starts a new game", %{player1: player1} do
-      game = GamePlay.new_game(player1)
+    test "starts a new game", %{player1: player} do
+      game = GamePlay.new_game(player)
       assert game.player1.name === "Heritier"
       assert game.player1.key === "X"
+    end
+  end
+
+  describe "&join/2" do
+    setup [:create_game]
+
+    test "joins a game", %{player2: player, game: game} do
+      game = GamePlay.join(game, player)
+      assert game.player2.name === "Kevin"
+      assert game.player2.key === "O"
     end
   end
 
--- test/tic_tac_toe/manager/game_supervisor_test.exs
+++ test/tic_tac_toe/manager/game_supervisor_test.exs
@@ -10,11 +10,32 @@ defmodule TicTacToe.Manager.GameSupervisorTest do
     {:ok, %{player1: player1}}
   end
 
-  describe "&start_child/2" do
+  describe "&start_child/1" do
     test "starts a child process", %{player1: player1} do
       GameSupervisor.start_child(player1)
       game = GameServer.get_status(:"#{player1.name}")
       assert game.player1.name === "Ntwali"
+
+      assert DynamicSupervisor.count_children(GameSupervisor) === %{
+               active: 1,
+               specs: 1,
+               supervisors: 0,
+               workers: 1
+             }
+    end
+  end
+
+  describe "&stop_game/1" do
+    test "stops a game process", %{player1: player1} do
+      GameSupervisor.start_child(player1)
+      GameSupervisor.stop_game(player1.name)
+
+      assert DynamicSupervisor.count_children(GameSupervisor) === %{
+               active: 0,
+               specs: 0,
+               supervisors: 0,
+               workers: 0
+             }
     end
   end
 end
